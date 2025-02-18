b6331402eed1e8589f00d19bc6cd913fa97e5236
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-02-18 22:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (41.87%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -7,19 +7,16 @@ class Solution {
         List<Integer> result = new ArrayList<>();
         
         while (top <= bottom && left <= right) {
-            // Traverse from left to right
             for (int i = left; i <= right; i++) {
                 result.add(matrix[top][i]);
             }
             top++;
 
-            // Traverse from top to bottom
             for (int i = top; i <= bottom; i++) {
                 result.add(matrix[i][right]);
             }
             right--;
 
-            // Traverse from right to left (only if there are remaining rows)
             if (top <= bottom) {
                 for (int i = right; i >= left; i--) {
                     result.add(matrix[bottom][i]);
@@ -27,7 +24,6 @@ class Solution {
                 bottom--;
             }
 
-            // Traverse from bottom to top (only if there are remaining columns)
             if (left <= right) {
                 for (int i = bottom; i >= top; i--) {
                     result.add(matrix[i][left]);
