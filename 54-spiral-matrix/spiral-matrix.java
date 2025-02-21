5d9dc2240bd65967fcbedc1f78d39a302dc40879
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-02-21 20:23:20 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (8.73%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.java
+++ 54-spiral-matrix/spiral-matrix.java
@@ -3,41 +3,36 @@ class Solution {
         int rowstart =0, rowend = matrix.length-1;
         int colstart =0, colend = matrix[0].length-1;
          List<Integer> ans = new ArrayList<>();
-        while(rowstart<=rowend && colstart<=colend){
-            for(int i = colstart; i<=colend; i++){
-                ans.add(matrix[rowstart][i]);
+         while(rowstart<=rowend && colstart<=colend){
 
-            }
-            rowstart++;
-            for(int i =rowstart; i<=rowend; i++){
-                ans.add(matrix[i][colend]);
-            }
-            colend--;
+             for(int i = colstart; i<=colend; i++){
+            ans.add(matrix[rowstart][i]);
+        }
+        rowstart++;
+        for(int i=rowstart; i<= rowend; i++){
+            ans.add(matrix[i][colend]);
+        }
+        colend--;
+        if(rowstart<=rowend){
+            for(int i = colend; i>=colstart; i--){
+            ans.add(matrix[rowend][i]);
+        }
+        rowend--;
 
-            if(rowstart<=rowend){
+        }
+        if(colstart<=colend){
 
-                for(int i = colend; i>=colstart; i--){
-                ans.add(matrix[rowend][i]);
-
-            }
-            rowend--;
-
-            }
-
-            if(colstart<=colend){
-
-                for(int i = rowend; i>=rowstart; i--){
-                ans.add(matrix[i][colstart]);
-
-            }
-            colstart++;
-
-            }
-            
+            for(int i=rowend; i>=rowstart; i--){
+            ans.add(matrix[i][colstart]);
+        }
+        colstart++;
 
 
         }
-        return ans;
+        
+         }
+         return ans;
+       
         
     }
 }
