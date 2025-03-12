cb545bbe3455c24d88c3edcc966c0d3f5316efc0
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-03-12 21:41:25 +0530

    Time: 6 ms (24.35%) | Memory: 45.1 MB (24.15%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+        int n =0;
+        for(int i=0 ; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+             if( i+1 < s.length() && map.get(s.charAt(i+1))> map.get(ch)){
+                n += map.get(s.charAt(i+1)) - map.get(ch);
+                i++;
+
+             }else{
+                n+=map.get(ch);
+             }
+
+        }
+        return n;
+
+       
+        
+    }
+}
