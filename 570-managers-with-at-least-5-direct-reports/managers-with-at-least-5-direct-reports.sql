6311603d06c2912273249fd8c74ad56134dac012
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-05-17 20:47:07 +0530

    Time: 340 ms (79.16%) | Memory: 0B (100.00%) - LeetSync

---
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select e.name 
+from Employee as e
+where id in (
+    select managerId 
+    from Employee
+    where managerId is not null
+    group by managerId
+    having count(*)>=5
+
+);
