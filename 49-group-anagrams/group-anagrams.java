94a0fc74414ef8354cbe576ae76dbaad9a8b96da
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-06-14 09:31:43 +0530

    Time: 13 ms (30.82%) | Memory: 48.7 MB (15.32%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,16 +1,33 @@
+import java.util.*;
+import java.math.BigInteger;
+
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> mpp = new HashMap<>();
-        for(String s : strs){
-            char[] ch= s.toCharArray();
-            Arrays.sort(ch);
-            String s1 = String.valueOf(ch);
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
 
-            if(!mpp.containsKey(s1)){
-                mpp.put(s1, new ArrayList<>());
+        // Prime numbers for 'a' to 'z'
+        int[] nos = {
+            2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+            31, 37, 41, 43, 47, 53, 59, 61, 67,
+            71, 73, 79, 83, 89, 97, 101
+        };
+
+        Map<BigInteger, List<String>> map = new HashMap<>();
+
+        for (String word : strs) {
+            BigInteger prod = BigInteger.ONE;
+            for (int i = 0; i < word.length(); i++) {
+                int prime = nos[word.charAt(i) - 'a'];
+                prod = prod.multiply(BigInteger.valueOf(prime));
             }
-            mpp.get(s1).add(s);
+
+            if (!map.containsKey(prod)) {
+                map.put(prod, new ArrayList<>());
+            }
+            map.get(prod).add(word);
         }
-        return new ArrayList<>(mpp.values());        
+
+        return new ArrayList<>(map.values());
     }
 }
