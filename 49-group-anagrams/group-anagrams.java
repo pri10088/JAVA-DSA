7fbb6b56851e347f2d1a0f87744196b7a947e6c9
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-07-13 17:00:15 +0530

    Time: 8 ms (39.37%) | Memory: 48.1 MB (35.44%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,33 +1,32 @@
-import java.util.*;
-import java.math.BigInteger;
+
 
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0)
-            return new ArrayList<>();
-
-        // Prime numbers for 'a' to 'z'
-        int[] nos = {
-            2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-            31, 37, 41, 43, 47, 53, 59, 61, 67,
-            71, 73, 79, 83, 89, 97, 101
-        };
-
-        Map<BigInteger, List<String>> map = new HashMap<>();
-
-        for (String word : strs) {
-            BigInteger prod = BigInteger.ONE;
-            for (int i = 0; i < word.length(); i++) {
-                int prime = nos[word.charAt(i) - 'a'];
-                prod = prod.multiply(BigInteger.valueOf(prime));
-            }
-
-            if (!map.containsKey(prod)) {
-                map.put(prod, new ArrayList<>());
-            }
-            map.get(prod).add(word);
+    public String getSignature(String s) {
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
         }
 
-        return new ArrayList<>(map.values());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                sb.append((char) ('a' + i)).append(count[i]);
+            }
+        }
+        return sb.toString();
+    }
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        Map<String, List<String>> groups = new HashMap<>();
+
+        for (String s : strs) {
+            groups.computeIfAbsent(getSignature(s), k -> new ArrayList<>()).add(s);
+        }
+
+        result.addAll(groups.values());
+
+        return result;
     }
 }
+
