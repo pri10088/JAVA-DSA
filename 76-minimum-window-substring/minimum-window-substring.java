ea9b18af6fee1fa6f3c62defe646fb31063295a6
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-07-20 11:40:30 +0530

    Time: 2 ms (99.83%) | Memory: 44.9 MB (67.28%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -1,33 +1,35 @@
 class Solution {
     public String minWindow(String s, String t) {
-        Map<Character, Integer> need = new HashMap<>();
-        for(char c : t.toCharArray()){
-            need.put(c, need.getOrDefault(c,0)+1);
+        if (s == null || t == null || s.length() == 0 || t.length() == 0 ||
+                s.length() < t.length()) {
+            return new String();
         }
-        Map<Character, Integer> window = new HashMap<>();
-        int left = 0,minLen=Integer.MAX_VALUE, have=0, needSize=need.size(),start = 0;
-        for(int right=0; right<s.length(); right++){
-            char c = s.charAt(right);
-            window.put(c, window.getOrDefault(c,0)+1);
-
-            if(need.containsKey(c) && window.get(c).intValue() == need.get(c).intValue()){
-                have++;
-            }
-            while(have == needSize){
-                if(right-left+1 < minLen){
-                    minLen = right-left+1;
-                    start = left;
-                }
-
-                char leftChar = s.charAt(left);
-                window.put(leftChar, window.get(leftChar)-1);
-                if(need.containsKey(leftChar) && window.get(leftChar)< need.get(leftChar)){
-                    have--;
-                }
-                left++;
-            }
-
+        int[] map = new int[128];
+        int count = t.length();
+        int start = 0, end = 0, minLen = Integer.MAX_VALUE, startIndex = 0;
+        /// UPVOTE !
+        for (char c : t.toCharArray()) {
+            map[c]++;
         }
-         return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
+
+        char[] chS = s.toCharArray();
+
+        while (end < chS.length) {
+            if (map[chS[end++]]-- > 0) {
+                count--;
+            }
+            while (count == 0) {
+                if (end - start < minLen) {
+                    startIndex = start;
+                    minLen = end - start;
+                }
+                if (map[chS[start++]]++ == 0) {
+                    count++;
+                }
+            }
+        }
+
+        return minLen == Integer.MAX_VALUE ? new String() :
+                new String(chS, startIndex, minLen);
     }
 }
