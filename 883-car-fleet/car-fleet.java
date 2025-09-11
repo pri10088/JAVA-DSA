066afbca278f9aae04fdd5b7f307e69d5d7f18a7
Shanmuga Priya AR <161346022+pri10088@users.noreply.github.com>
2025-09-11 11:42:10 +0530

    Time: 84 ms (51.07%) | Memory: 59.1 MB (26.21%) - LeetSync

--- 883-car-fleet/car-fleet.java
+++ 883-car-fleet/car-fleet.java
@@ -1,17 +1,33 @@
+
 class Solution {
-        public int carFleet(int target, int[] pos, int[] speed) {
-        int N = pos.length, res = 0;
-        double[][] cars = new double[N][2];
-        for (int i = 0; i < N; ++i)
-            cars[i] = new double[] {pos[i], (double)(target - pos[i]) / speed[i]};
-        Arrays.sort(cars, (a, b) -> Double.compare(a[0], b[0]));
-        double cur = 0;
-        for (int i = N - 1; i >= 0 ; --i) {
-            if (cars[i][1] > cur) {
-                cur = cars[i][1];
-                res++;
-            }
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
+        if (n == 0) return 0;
+
+        // Pair cars: [position, timeToTarget]
+        double[][] cars = new double[n][2];
+        for (int i = 0; i < n; i++) {
+            cars[i][0] = position[i];
+            cars[i][1] = (double)(target - position[i]) / speed[i];
         }
-        return res;
+
+        // Sort cars by starting position descending (closer to target first)
+        Arrays.sort(cars, (a, b) -> Double.compare(b[0], a[0]));
+
+        int fleets = 0;
+        double curTime = 0.0;
+
+        // Traverse from front-most to back-most
+        for (int i = 0; i < n; i++) {
+            double time = cars[i][1];
+            if (time > curTime) { 
+                // new fleet formed
+                fleets++;
+                curTime = time; 
+            }
+            // else: merges into fleet ahead
+        }
+
+        return fleets;
     }
 }
